8a0e35cb3f4b88dd528b8c59d40b842d1cacd387
Kazhem <m.kazhemskiy@gmail.com>
2015-04-05 20:19:25 +0300

    Updated Option menu

--- gui/hide.py
+++ gui/hide.py
@@ -20,6 +20,7 @@ class Hide:
         st.configure('MY.TFrame')
 
         self.on_main_frame()
+        self.commands_for_rbuttons()
 
         self.slave.wait_window()
 
@@ -77,11 +78,11 @@ class Hide:
         self.hiding_stegomethod_video = tkinter.Frame(self.option_frame, height=100, width=350, bd=1)
         self.hiding_stegomethod_video.place(x=0, y=320, width=350, height=100)  # Рамка для выбора метода скрытия в видео
 
-        self.hiding_algorithm_video = tkinter.Frame(self.option_frame, height=100, width=350, bd=1)
-        self.hiding_algorithm_video.place(x=0, y=420, width=350, height=100)  # Рамка для выбора алгоритма скрытия в видео
+        self.hiding_algorithm_video = tkinter.Frame(self.option_frame, height=120, width=350, bd=1)
+        self.hiding_algorithm_video.place(x=0, y=420, width=350, height=120)  # Рамка для выбора алгоритма скрытия в видео
 
         self.hiding_method_audio = tkinter.Frame(self.option_frame, height=80, width=350, bd=1)
-        self.hiding_method_audio.place(x=0, y=520, width=350, height=80)  # Рамка для выбора метода скрытия в аудио
+        self.hiding_method_audio.place(x=0, y=540, width=350, height=100)  # Рамка для выбора метода скрытия в аудио
 
 
 
@@ -159,30 +160,33 @@ class Hide:
         """
 
 
-        self.var_cont_type = IntVar()
+        self.var_cont_type = StringVar()
         self.label1 = Label(self.cont_type, text='Тип контейнера:', style='BW.TLabel')
-        self.rbutton1 = Radiobutton(self.cont_type, text='Видео', variable=self.var_cont_type, value=1, command=self.command_click_on_var_cont_type())
+        self.rbutton1 = Radiobutton(self.cont_type, text='Видео', variable=self.var_cont_type, value='video')
+        self.rbutton2 = Radiobutton(self.cont_type, text='Аудио', variable=self.var_cont_type, value='audio')
 
 
+        self.rbutton2.configure(state=DISABLED)
 
-        self.rbutton2 = Radiobutton(self.cont_type, text='Аудио', variable=self.var_cont_type, value=2)
 
         self.label1.place(x=0, y=0)
         self.rbutton1.place(x=10, y=20)
         self.rbutton2.place(x=10, y=40)
+
+
 
     def sub_add_on_file_type(self):
         """
         Рамка для выбора типа скрываемого файла
         """
 
-        self.var_file_type = IntVar()
+        self.var_file_type = StringVar()
         self.label2 = Label(self.file_type, text='Тип скрываемого файла:', style='BW.TLabel')
 
         self.rbutton3 = Radiobutton(self.file_type, text='Цифровой Водяной Знак',
-                                            variable=self.var_file_type, value=1)
+                                            variable=self.var_file_type, value='cvz')
         self.rbutton4 = Radiobutton(self.file_type, text='Текстовый файл',
-                                            variable=self.var_file_type, value=2)
+                                            variable=self.var_file_type, value='text')
 
         self.label2.place(x=0, y=0)
         self.rbutton3.place(x=10, y=20)
@@ -193,14 +197,17 @@ class Hide:
         Рамка для выбора способа сжатия в видеоконтейнере
         """
 
-        self.var_comp_method_vid = IntVar()
+        self.var_comp_method_vid = StringVar()
         self.label3 = Label(self.compression_method_video, text='Тип сжатия в видеоконтейнере:', style='BW.TLabel')
         self.rbutton5 = Radiobutton(self.compression_method_video, text='bmp',
-                                            variable=self.var_comp_method_vid, value=1)
+                                            variable=self.var_comp_method_vid, value='bmp')
         self.rbutton6 = Radiobutton(self.compression_method_video, text='jpeg',
-                                            variable=self.var_comp_method_vid, value=2)
+                                            variable=self.var_comp_method_vid, value='jpeg')
         self.rbutton7 = Radiobutton(self.compression_method_video, text='mpeg',
-                                            variable=self.var_comp_method_vid, value=3)
+                                            variable=self.var_comp_method_vid, value='mpeg')
+
+        self.rbutton6.configure(state=DISABLED)
+        self.rbutton7.configure(state=DISABLED)
 
         self.label3.place(x=0, y=0)
         self.rbutton5.place(x=10, y=20)
@@ -213,12 +220,15 @@ class Hide:
         """
 
 
-        self.var_comp_method_audio = IntVar()
+        self.var_comp_method_audio = StringVar()
         self.label4 = Label(self.compression_method_audio, text='Тип сжатия в аудиоконтейнере:', style='BW.TLabel')
         self.rbutton8 = Radiobutton(self.compression_method_audio, text='waw',
-                                            variable=self.var_comp_method_audio, value=1)
-        self.rbutton9 = Radiobutton(self.compression_method_audio, text='Mp4',
-                                            variable=self.var_comp_method_audio, value=2)
+                                            variable=self.var_comp_method_audio, value='waw')
+        self.rbutton9 = Radiobutton(self.compression_method_audio, text='mp4',
+                                            variable=self.var_comp_method_audio, value='mp4')
+
+        self.rbutton8.configure(state=DISABLED)
+        self.rbutton9.configure(state=DISABLED)
 
         self.label4.place(x=0, y=0)
         self.rbutton8.place(x=10, y=20)
@@ -229,14 +239,14 @@ class Hide:
         Рамка для способа скрытия в видео
         """
 
-        self.var_hiding_method_vid = IntVar()
+        self.var_hiding_method_vid = StringVar()
         self.label5 = Label(self.hiding_method_video, text='Способ скрытия информации в видеоконтейнере:',
                             style='BW.TLabel')
         self.rbutton10 = Radiobutton(self.hiding_method_video, text='В пространственной области',
-                                             variable=self.var_hiding_method_vid, value=1)
+                                             variable=self.var_hiding_method_vid, value='spatial')
         self.rbutton11 = Radiobutton(self.hiding_method_video, text='В частотной области',
-                                             variable=self.var_hiding_method_vid, value=2)
-        self.rbutton10.configure(state=DISABLED)
+                                             variable=self.var_hiding_method_vid, value='frequency')
+
 
 
         self.label5.place(x=0, y=0)
@@ -248,21 +258,21 @@ class Hide:
         Рамка для метода скрытия в видео
         """
 
-        self.var_hiding_stegomethod_vid = IntVar()
+        self.var_hiding_stegomethod_vid = StringVar()
         self.label6 = Label(self.hiding_stegomethod_video, text='Метод скрытия информации в видеоконтейнере:',
                             style='BW.TLabel')
         self.rbutton12 = Radiobutton(self.hiding_stegomethod_video,
                                              text='Метод кодирования наименее значащих бит',
-                                             variable=self.var_hiding_stegomethod_vid, value=1)
+                                             variable=self.var_hiding_stegomethod_vid, value='LSBvid')
         self.rbutton13 = Radiobutton(self.hiding_stegomethod_video,
                                              text='Метод псевдослучайного интервала',
-                                             variable=self.var_hiding_stegomethod_vid, value=2)
+                                             variable=self.var_hiding_stegomethod_vid, value='PRI')
         self.rbutton14 = Radiobutton(self.hiding_stegomethod_video,
                                              text='Метод псевдослучайной перестановки',
-                                             variable=self.var_hiding_stegomethod_vid, value=3)
+                                             variable=self.var_hiding_stegomethod_vid, value='PRP')
         self.rbutton15 = Radiobutton(self.hiding_stegomethod_video,
                                              text='Метод Коха и Жао',
-                                             variable=self.var_hiding_stegomethod_vid, value=4)
+                                             variable=self.var_hiding_stegomethod_vid, value='K&J')
 
         self.label6.place(x=0, y=0)
         self.rbutton12.place(x=10, y=20)
@@ -275,31 +285,38 @@ class Hide:
         Рамка для алгоритма скрытия в видео
         """
 
-        self.var_hiding_algorithm_vid = IntVar()
+        self.var_hiding_algorithm_vid = StringVar()
         self.label20 = Label(self.hiding_algorithm_video, text='Алгоритм скрытия информации в видеоконтейнере:',
                              style='BW.TLabel')
         self.rbutton18 = Radiobutton(self.hiding_algorithm_video,
                                              text='Алгоритм Куттера',
-                                             variable=self.hiding_algorithm_video, value=1)
+                                             variable=self.var_hiding_algorithm_vid, value='Kutter')
         self.rbutton19 = Radiobutton(self.hiding_algorithm_video,
                                              text='Алгоритм Брайндокса',
-                                             variable=self.hiding_algorithm_video, value=2)
+                                             variable=self.var_hiding_algorithm_vid, value='Braindoks')
         self.rbutton20 = Radiobutton(self.hiding_algorithm_video,
                                              text='Алгоритм Дармстедтера-Делейгла',
-                                             variable=self.hiding_algorithm_video, value=3)
+                                             variable=self.var_hiding_algorithm_vid, value='D-D')
         self.rbutton21 = Radiobutton(self.hiding_algorithm_video,
                                              text='Алгоритм Фридриха',
-                                             variable=self.hiding_algorithm_video, value=4)
+                                             variable=self.var_hiding_algorithm_vid, value='Fridrih')
 
         self.rbutton22 = Radiobutton(self.hiding_algorithm_video,
                                              text='Алгоритм Хсу и Ву',
-                                             variable=self.hiding_algorithm_video, value=5)
+                                             variable=self.var_hiding_algorithm_vid, value='H&V')
+
+        self.rbutton18.configure(state=DISABLED)
+        self.rbutton19.configure(state=DISABLED)
+        self.rbutton20.configure(state=DISABLED)
+        self.rbutton21.configure(state=DISABLED)
+        self.rbutton22.configure(state=DISABLED)
 
         self.label20.place(x=0, y=0)
         self.rbutton18.place(x=10, y=20)
         self.rbutton19.place(x=10, y=40)
         self.rbutton20.place(x=10, y=60)
         self.rbutton21.place(x=10, y=80)
+        self.rbutton22.place(x=10, y=100)
 
 
     def sub_add_on_hiding_method_audio(self):
@@ -307,21 +324,25 @@ class Hide:
         Рамка для метода скрытия в аудио
         """
 
-        self.var_hiding_method_audio = IntVar()
+        self.var_hiding_method_audio = StringVar()
         self.label7 = Label(self.hiding_method_audio, text='Метод скрытия информации в аудиоконтейнере:',
                             style='BW.TLabel')
-        self.rbutton15 = Radiobutton(self.hiding_method_audio,
+        self.rbutton23 = Radiobutton(self.hiding_method_audio,
                                              text='Метод кодирования наименее значащих бит',
-                                             variable=self.var_hiding_method_audio, value=1)
+                                             variable=self.var_hiding_method_audio, value='LSBaudio')
         self.rbutton16 = Radiobutton(self.hiding_method_audio,
                                              text='Метод расширения спектра',
-                                             variable=self.var_hiding_method_audio, value=2)
+                                             variable=self.var_hiding_method_audio, value='expand')
         self.rbutton17 = Radiobutton(self.hiding_method_audio,
                                              text='Метод фазового кодирования',
-                                             variable=self.var_hiding_method_audio, value=3)
+                                             variable=self.var_hiding_method_audio, value='phase')
+
+        self.rbutton23.configure(state=DISABLED)
+        self.rbutton16.configure(state=DISABLED)
+        self.rbutton17.configure(state=DISABLED)
 
         self.label7.place(x=0, y=0)
-        self.rbutton15.place(x=10, y=20)
+        self.rbutton23.place(x=10, y=20)
         self.rbutton16.place(x=10, y=40)
         self.rbutton17.place(x=10, y=60)
 
@@ -400,12 +421,106 @@ class Hide:
         self.label17.pack()
 
 
-    def command_click_on_var_cont_type(self):
-        if self.var_cont_type == 1:
-            self.rbutton8.state(DISABLED)
-            self.rbutton8.state(DISABLED)
+    def commands_for_rbuttons(self):
+        self.rbutton1.configure(command=self.command_for_rbutton1)
+        self.rbutton2.configure(command=self.command_for_rbutton2)
+        self.rbutton3.configure(command=self.command_for_rbutton3)
+        self.rbutton4.configure(command=self.command_for_rbutton4)
+        self.rbutton5.configure(command=self.command_for_rbutton5)
+        self.rbutton6.configure(command=self.command_for_rbutton6)
+        self.rbutton7.configure(command=self.command_for_rbutton7)
+        self.rbutton8.configure(command=self.command_for_rbutton8)
+        self.rbutton9.configure(command=self.command_for_rbutton9)
+        self.rbutton10.configure(command=self.command_for_rbutton10)
+        self.rbutton11.configure(command=self.command_for_rbutton11)
+        self.rbutton12.configure(command=self.command_for_rbutton12)
+        self.rbutton13.configure(command=self.command_for_rbutton13)
+        self.rbutton14.configure(command=self.command_for_rbutton14)
+        self.rbutton15.configure(command=self.command_for_rbutton15)
+        self.rbutton16.configure(command=self.command_for_rbutton16)
+        self.rbutton17.configure(command=self.command_for_rbutton17)
+        self.rbutton18.configure(command=self.command_for_rbutton18)
+        self.rbutton19.configure(command=self.command_for_rbutton19)
+        self.rbutton20.configure(command=self.command_for_rbutton20)
+        self.rbutton21.configure(command=self.command_for_rbutton21)
+        self.rbutton22.configure(command=self.command_for_rbutton22)
+        self.rbutton23.configure(command=self.command_for_rbutton23)
 
+    def command_for_rbutton1(self):
+        pass
+
+    def command_for_rbutton2(self):
+        pass
+
+    def command_for_rbutton3(self):
+        pass
+
+    def command_for_rbutton4(self):
+        pass
+
+    def command_for_rbutton5(self):
+        pass
+
+    def command_for_rbutton6(self):
+        pass
+
+    def command_for_rbutton7(self):
+        pass
+
+    def command_for_rbutton8(self):
+        pass
+
+    def command_for_rbutton9(self):
+        pass
     
+    def command_for_rbutton10(self):
+        self.rbutton12.configure(state=NORMAL)
+        self.rbutton13.configure(state=NORMAL)
+        self.rbutton14.configure(state=NORMAL)
+        self.rbutton15.configure(state=DISABLED)
+
+
+    def command_for_rbutton11(self):
+        self.rbutton12.configure(state=DISABLED)
+        self.rbutton13.configure(state=DISABLED)
+        self.rbutton14.configure(state=DISABLED)
+        self.rbutton15.configure(state=NORMAL)
+
+    def command_for_rbutton12(self):
+        pass
+
+    def command_for_rbutton13(self):
+        pass
+
+    def command_for_rbutton14(self):
+        pass
+
+    def command_for_rbutton15(self):
+        pass
+
+    def command_for_rbutton16(self):
+        pass
+
+    def command_for_rbutton17(self):
+        pass
+
+    def command_for_rbutton18(self):
+        pass
+    
+    def command_for_rbutton19(self):
+        pass
+
+    def command_for_rbutton20(self):
+        pass
+
+    def command_for_rbutton21(self):
+        pass
+
+    def command_for_rbutton22(self):
+        pass
+
+    def command_for_rbutton23(self):
+        pass
         
         
 
@@ -415,6 +530,6 @@ class Hide:
 
     
         
-            
+
     
         
